445e7fc0a5b9a2cf10eec21e13dc75a8cd95f58b
Ryan Canton <ryancanton999@gmail.com>
2023-01-16 14:13:57 -0700

    Test: Add test for user story 5

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe 'A Bulk Discount Show Page' do
     expect(page).to have_content(bulk_discount_1.percentage_discount)
     expect(page).to have_content(bulk_discount_1.quantity_threshold)
   end
+
+  it 'contains a link that allows you to edit the discount' do
+    merchant1 = Merchant.create!(name: 'Hair Care')
+    bulk_discount_1 = merchant1.bulk_discounts.create!(percentage_discount: 25, quantity_threshold: 30)
+
+    visit merchant_bulk_discount_path(merchant1.id, bulk_discount_1.id)
+
+    expect(page).to have_link("Edit Bulk Discount")
+    click_link("Edit Bulk Discount")
+    expect(current_path).to eq(edit_merchant_bulk_discount(merchant1.id, bulk_discount_1.id))
+
+    expect(page).to have_field(:percentage_discount, with: bulk_discount_1.percentage_discount)
+    expect(page).to have_field(:quantity_threshold, with: bulk_discount_1.quantity_threshold)
+
+    fill_in :percentage_discount, with: '35'
+    click_button 'save'
+
+    expect(current_path).to eq(merchant_bulk_discount_path(merchant1.id, bulk_discount_1.id))
+    expect(page).to have_content("Percentage Discount: 35")
+  end
 end
