3a968801c2bedac5d5cf3f4d5537d8c82e68463d
Ryan Canton <ryancanton999@gmail.com>
2023-01-17 11:24:22 -0700

    Test/Feat: Added test and feature for user story 8

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -17,6 +17,8 @@ describe 'Admin Invoices Index Page' do
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
     @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
 
+    @bd1 = @m1.bulk_discounts.create!(percentage_discount: 25, quantity_threshold: 10)
+
     visit admin_invoice_path(@i1)
   end
 
@@ -68,5 +70,11 @@ describe 'Admin Invoices Index Page' do
       expect(current_path).to eq(admin_invoice_path(@i1))
       expect(@i1.status).to eq('completed')
     end
+  end
+
+  it "shows the total revenue and discounted revenue for the invoice" do
+
+    expect(page).to have_content(@i1.total_revenue)
+    expect(page).to have_content(@i1.total_discounted_revenue)
   end
 end
